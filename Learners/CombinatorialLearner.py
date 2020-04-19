71442d1c21bcd1eb3199f0f9e770d87c8c246713
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-04-19 18:23:32 +0200

    development of method knapsack_solver

--- Learners/CombinatorialLearner.py
+++ Learners/CombinatorialLearner.py
@@ -1,3 +1,5 @@
+import itertools
+import numpy as np
 
 
 class CombinatorialLearner:
@@ -7,57 +9,75 @@ class CombinatorialLearner:
     ----------
     __budget_env : list[] (of BudgetEnvironment)
         represent the subcampaigns
-    __GP_learner : list[] (of GPTSLearner)
+    __gp_learner : list[] (of GPTSLearner)
         represents the learner for each subcampaigns
     __daily_budget : float
         the total daily budget for the campaign
     """
-    def __init__(self, budget_environments, GP_learner, budget):
+    def __init__(self, budget_environments, gp_learner, budget):
         self.__budget_env = budget_environments
-        self.__GP_learner = GP_learner
-        self.__daily_budgets = budget
+        self.__gp_learner = gp_learner
+        self.__daily_budget = budget
+        self.__super_arms = [x for x in itertools.product(*self.__set_super_arms()) if sum(x) <= self.__daily_budget]
+
+    def __set_super_arms(self):
+        return [x.arms for x in self.__gp_learner]
 
     def collect_sample(self):
         """
         Returns
         ----------
-        sample : list[]
+        sample : list[] (list of dictionaries)
             the expected number of click w.r.t the budgets for each subcampaigns
         """
         sample = []
-        for gpl in self.__GP_learner:
+        for gpl in self.__gp_learner:
             sample = sample.append(gpl.pull_arms)
         return sample
 
     def knapsacks_solver(self):
-        pass
-        #return superArm
+        """
+        Returns
+        ----------
+        best_super_arm : tuple
+            the best super arm selected at the current round
+        """
+        samples = self.collect_sample()
+        reward_list = np.array([])
+        for s_arm in self.__super_arms:
+            reward = 0
+            for idx in range(0, len(s_arm)):
+                reward += samples[idx][s_arm[idx]]
+            reward_list = np.append(reward_list, reward)
 
-    def get_realization(self, superArm):
+        best_super_arm = self.__super_arms[int(np.argmax(reward_list))]
+        return best_super_arm
+
+    def get_realization(self, super_arm):
         """
         Parameters
-        superArm : lis[]
-            best superarm
+        super_arm : tuple
+            best super-arm
         ----------
         Returns
         ----------
         reward : list[]
-            the reward of the arm selected (one for each subcampigns)
+            the reward of the arm selected (one for each sub campaign)
         """
         reward = []
-        for i in range(0,len(self.__budget_env)):
-            reward = reward.append(self.__budget_env[i].round(superArm[i]))
+        for i in range(0, len(self.__budget_env)):
+            reward.append(self.__budget_env[i].round(super_arm[i]))
         return reward
 
-    def update(self, superArm, reward):
+    def update(self, super_arm, reward):
         """
         Updates the model
         Parameters
         ----------
-        superArm : list[]
-            best superarm
+        super_arm : list[]
+            best super-arm
         reward : list[]
             reward of the arm selected
         """
-        for i in range(0, len(self.__GP_learner)):
-            self.__GP_learner[i].update(superArm[i], reward[i])
+        for i in range(0, len(self.__gp_learner)):
+            self.__gp_learner[i].update(super_arm[i], reward[i])
