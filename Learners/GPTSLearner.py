e26ea62b1e6f156708573544dad0cb0702786eb6
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-04-20 17:00:17 +0200

    clas documentation of GPTSLearner class

--- Learners/GPTSLearner.py
+++ Learners/GPTSLearner.py
@@ -5,6 +5,27 @@ from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 
 
 class GPTSLearner(Learner):
+    """
+    It is the Thompson Sampling algorithm.
+    It uses regression through Gaussian Process in learning phase.
+    Attributes
+    ----------
+     arms : list
+        it is the list of the arms of the bandit algorithm
+    __means : numpy.array
+        it is the array which contains the means of the distribution associated to each arm
+    __std : numpy.array
+       it is the array which contains the standard deviations of the distribution associated to each arm
+    __pulled_arms : list
+        it is a list of the arms pulled during the learning process( they are stored in chronological order)
+    __alpha : float
+        it is the standard deviation of the noise affecting the data
+    __kernel :
+        it is the kernel of the Gaussian Process
+    __gp: GaussianProcessRegressor
+        it is the Gaussian Process regressor itself
+    """
+    
     def __init__(self, n_arms, arms, noise_std):
         super.__init__(n_arms)
         self.arms = arms
@@ -17,21 +38,41 @@ class GPTSLearner(Learner):
                                              n_restarts_optimizer=9)
 
     def __update_observations(self, arm_idx, reward):
+        """
+
+        :param arm_idx:
+        :param reward:
+        :return:
+        """
         self.update_observations(arm_idx, reward)
         self.__pulled_arms.append(self.arms[arm_idx])
 
     def __update_model(self):
+        """
+
+        :return:
+        """
         x = np.atleast_2d(self.__pulled_arms).T
         y = self._collected_rewards
         self.__gp.fit(x, y)
         self.__means, self.__std = self.gp.predict(np.atleast_2d(self.arms).T, return_std=True)
 
     def update(self, pulled_arm, reward):
+        """
+
+        :param pulled_arm:
+        :param reward:
+        :return:
+        """
         self._round += 1
         self.__update_observations(pulled_arm, reward)
         self.__update_model()
 
     def pull_arms(self):
+        """
+
+        :return:
+        """
         sampled_values = np.random.normal(self.__means, self.__std)
         sample_dic = {self.__arms[x]: sampled_values[x] for x in range(0, self._n_arms)}
         return sample_dic
