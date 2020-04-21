50a21609d634093a8cf2a6cff889525f207d5c75
Matteo Orsolini <matteo.orsolini97@gmail.com>
2020-04-21 23:20:45 +0200

    adding methods to read file json

--- SystemConfiguration.py
+++ SystemConfiguration.py
@@ -5,7 +5,8 @@ class SystemConfiguration:
     """
     A class which set the parameters reading from configuration.json
     """
-    def __init__(self):
+
+    def __init__(self, path):
         with open("configuration.json") as json_config:
             self.__data_config = json.load(json_config)
         json_config.close()
@@ -13,8 +14,23 @@ class SystemConfiguration:
     def init_function(self, function_name):
         """
         :param function_name : str, name of the function selected
-        :return: dictionary, the parameters of the function
+        :return: tuple = (bound, slope), the parameters of the function
         """
-        return self.__data_config["function"][function_name]
+        return (self.__data_config["function"][function_name]["bound"],
+                self.__data_config["function"][function_name]["slope"])
 
+    def init_sub_campaign(self, sub_campaign):
+        """
+        :param sub_campaign: str, name of the sub_campaign selected
+        :return: dictionary, the parameters of the sub_campaign
+        """
+        return self.__data_config["campaign"][sub_campaign]
 
+    def init_advertising_experiment2(self):
+        return self.__data_config["Advertising_experiment2"]
+
+    def init_noise(self):
+        return self.__data_config["campaign"]["sigma"]
+
+    def init_total_budget(self):
+        return self.__data_config["campaign"]["total_budget"]
