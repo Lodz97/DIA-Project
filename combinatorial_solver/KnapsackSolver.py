423c5107d4dcf25214e7d1952a3db0ff35f0aaa5
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-04-21 21:49:37 +0200

    made sub_campaign_attribute of type numpy.array

--- combinatorial_solver/KnapsackSolver.py
+++ combinatorial_solver/KnapsackSolver.py
@@ -12,13 +12,17 @@ class KnapsackSolver:
         self.budgets = np.unique(budget)                              # NB np.unique already returns the array sorted
 
     def __create_sub_campaigns_matrix(self, arms_dict):
-        self.sub_campaigns_matrix = []
+        matrix = []
         for dct in arms_dict:
             for b in self.budgets:
                 if b not in dct.keys():
                     dct.update({b: -np.inf})
-            self.sub_campaigns_matrix.append([dct[key] for key in sorted(dct.keys(), reverse=False)])
+            matrix.append([dct[key] for key in sorted(dct.keys(), reverse=False)])
+
+        self.sub_campaigns_matrix = np.array(matrix)
 
     def solve(self, arms_dict):
         self.__set_budgets(arms_dict)
         self.__create_sub_campaigns_matrix(arms_dict)
+
+        #matrix = np.empty((subcamp_matrix.shape[0] + 1, subcamp_matrix.shape[1]), dtype=Cell)
