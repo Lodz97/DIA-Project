1471883e71e6a461fcb5b4765f7f3de5a12b9818
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-04-21 18:39:55 +0200

    implementation of method create_sub_campaign in KnapsackSolver class

--- KnapsackSolver.py
+++ KnapsackSolver.py
@@ -5,9 +5,20 @@ class KnapsackSolver:
 
     def __init__(self, arms_dict):
         self.budgets = self.__set_budgets(arms_dict)
+        self.sub_campaigns_matrix = self.__create_sub_campaigns_matrix(arms_dict)
 
 
     def __set_budgets(self, arms_dict):
         budget = [element for sublist in arms_dict for element in sublist.keys()]
         budget = np.unique(budget)                              # NB np.unique already returns the array sorted
         return budget
+
+    def __create_sub_campaigns_matrix(self, arms_dict):
+        matrix = []
+        for dct in arms_dict:
+            for b in self.budgets:
+                if b not in dct.keys():
+                    dct.update({b: -np.inf})
+            matrix.append([dct[key] for key in sorted(dct.keys(), reverse=False)])
+
+        return matrix
