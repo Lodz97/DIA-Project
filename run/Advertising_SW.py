6883131b4b523f6ef40e50fc35122c229e43b4d8
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-04-27 18:49:28 +0200

    implementation of main method for sliding windows

--- run/Advertising_SW.py
+++ run/Advertising_SW.py
@@ -1,6 +1,22 @@
 import SystemConfiguration
 import numpy as np
 from environment.ClickFunction import ClickFunction
+from environment.NonStationaryBudgetEnvironment import NonStationaryBudgetEnvironment
+from Learners import GPTSLearner, SWGPTSLearner
+from combinatorial_solver.KnapsackSolver import KnapsackSolver
+from Learners.CombinatorialLearner import CombinatorialLearner
+import plot
+
+
+def get_optimum(dic_budget):
+    reward_non_stationary = []
+    k_sol = KnapsackSolver(dic_budget[0][0])
+    for x in range(0, len(dic_budget[0])):          # num of subcampaigns
+        for y in range(0, len(dic_budget)):
+            reward_non_stationary.append(k_sol.solve([dic_budget[y][x]]))
+
+    return reward_non_stationary
+
 
 if __name__ == "__main__":
 
@@ -14,7 +30,9 @@ if __name__ == "__main__":
     budget_sub_c3 = np.linspace(param_sub_c3["min_budget"], param_sub_c3["max_budget"], param_sub_c3["n_arms"])
 
     sigma = config.init_noise()
-
+    t_horizon = config.init_advertising_experiment3()["t_horizon"]
+    n_experiment = config.init_advertising_experiment3()["n_experiment"]
+    window_size = int(np.sqrt(t_horizon))
     func_c1_p1 = ClickFunction(*config.init_function("func_man_eu_p1"))
     func_c1_p2 = ClickFunction(*config.init_function("func_man_eu_p2"))
     func_c1_p3 = ClickFunction(*config.init_function("func_man_eu_p3"))
@@ -25,3 +43,47 @@ if __name__ == "__main__":
     func_c3_p2 = ClickFunction(*config.init_function("func_woman_p2"))
     func_c3_p3 = ClickFunction(*config.init_function("func_woman_p3"))
 
+    combinatorial_reward_experiment = []
+    sw_combinatorial_reward_experiment = []
+    campaign = []
+
+    for i in range(0, n_experiment):
+
+        sub_c1 = NonStationaryBudgetEnvironment(budget_sub_c1, sigma, [func_c1_p1, func_c1_p2, func_c1_p3], t_horizon)
+        sub_c2 = NonStationaryBudgetEnvironment(budget_sub_c2, sigma, [func_c2_p1, func_c2_p2, func_c2_p3], t_horizon)
+        sub_c3 = NonStationaryBudgetEnvironment(budget_sub_c3, sigma, [func_c3_p1, func_c3_p2, func_c3_p3], t_horizon)
+
+        campaign = [sub_c1, sub_c2, sub_c3]
+        theta, l_scale = config.init_learner_kernel()
+        gpts_l1 = GPTSLearner.GPTSLearner(len(budget_sub_c1), budget_sub_c1, sigma, theta, l_scale)
+        gpts_l2 = GPTSLearner.GPTSLearner(len(budget_sub_c2), budget_sub_c2, sigma, theta, l_scale)
+        gpts_l3 = GPTSLearner.GPTSLearner(len(budget_sub_c3), budget_sub_c3, sigma, theta, l_scale)
+        learners = [gpts_l1, gpts_l2, gpts_l3]
+
+        theta, l_scale = config.init_learner_kernel()
+        sw_gpts_l1 = SWGPTSLearner.SWGPTSLearner(len(budget_sub_c1), budget_sub_c1, sigma, theta, l_scale, window_size)
+        sw_gpts_l2 = SWGPTSLearner.SWGPTSLearner(len(budget_sub_c2), budget_sub_c2, sigma, theta, l_scale, window_size)
+        sw_gpts_l3 = SWGPTSLearner.SWGPTSLearner(len(budget_sub_c3), budget_sub_c3, sigma, theta, l_scale, window_size)
+
+        sw_learners = [sw_gpts_l1, sw_gpts_l2, sw_gpts_l3]
+
+        comb_learner = CombinatorialLearner(campaign, learners)
+        sw_comb_learner = CombinatorialLearner(campaign, sw_learners)
+
+        for t in range(0, t_horizon):
+            super_arm = comb_learner.knapsacks_solver()
+            rewards = comb_learner.get_realization(super_arm)
+            comb_learner.update(super_arm, rewards)
+
+            sw_super_arm = sw_comb_learner.knapsacks_solver()
+            sw_rewards = sw_comb_learner.get_realization(sw_super_arm)
+            sw_comb_learner.update(sw_super_arm, sw_rewards)
+
+        combinatorial_reward_experiment.append(comb_learner.collected_reward)
+        sw_combinatorial_reward_experiment.append(sw_comb_learner.collected_reward)
+        print(i)
+
+    optimum = get_optimum([campaign[0].list_clicks_budget, campaign[1].list_clicks_budget,
+                           campaign[2].list_clicks_budget])
+
+    plot.plot_regret_comparison(optimum, combinatorial_reward_experiment, sw_combinatorial_reward_experiment)
