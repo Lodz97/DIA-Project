f53f469f900190c39c04c8050b1d6780035b27f8
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-04-27 18:52:20 +0200

    fixed bug in method calling

--- environment/NonStationaryBudgetEnvironment.py
+++ environment/NonStationaryBudgetEnvironment.py
@@ -14,9 +14,9 @@ class NonStationaryBudgetEnvironment(BudgetEnvironment):
     """
     def __init__(self, budget, sigma, func, horizon):
         BudgetEnvironment.__init__(self, budget, sigma, func[0])
-        self.__list_clicks_budget = []
+        self.list_clicks_budget = []
         for x in range(0, len(func)):
-            self.__list_clicks_budget.append({budget[i]: func[x].apply_func(budget)[i] for i in range(0, len(budget))})
+            self.list_clicks_budget.append({budget[i]: func[x].apply_func(budget)[i] for i in range(0, len(budget))})
         self.__t_horizon = horizon
         self.__t = 0
         self.__current_phase = 0
@@ -25,14 +25,23 @@ class NonStationaryBudgetEnvironment(BudgetEnvironment):
         """
         Returns a stochastic reward given by the expected number of clicks and the noise of the current phase
         """
-        n_phases = len(self.__list_clicks_budget)
+        n_phases = len(self.list_clicks_budget)
         phase_size = self.__t_horizon/n_phases
         current_phase = int(self.__t/phase_size)
+
         if current_phase > self.__current_phase:
-            self.clicks_budget(self.__list_clicks_budget[current_phase])
+            self.clicks_budget = self.list_clicks_budget[current_phase]
             self.__current_phase += 1
-        self.__t += 1
+        self.__t += 0.5
         return super().round(pulled_arm)
+
+    @property
+    def list_clicks_budget(self):
+        return self.__list_clicks_budget
+
+    @list_clicks_budget.setter
+    def list_clicks_budget(self, list_clicks_budgets):
+        self.__list_clicks_budget = list_clicks_budgets
 
 
 
