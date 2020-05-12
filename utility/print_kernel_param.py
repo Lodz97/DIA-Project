08f8a3bc44d26e2a3a5a03bc2f2cf0e5ff331555
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-05-12 18:36:28 +0200

    removal of debug lines

--- utility/print_kernel_param.py
+++ utility/print_kernel_param.py
@@ -14,17 +14,17 @@ def write_param(string_param):
 
 if __name__ == '__main__':
 
-    config = SysConfAdvSW.SysConfAdvSW("/home/orso/Documents/POLIMI/DataIntelligenceApplication/DIA-Project/configuration/")
+    config = SysConfAdvSW.SysConfAdvSW("/home/mattia/PyProjects/DIA-Project/configuration/")
     func_list = list(chain(*config.function()))
     sigma = config.init_noise()
     name = config.function_name()
-    print(len(name))
     string = ''
 
     for i in range(0, len(func_list)):
         estimate = EstimateHyperparameters.EstimateHyperparameters(func_list[i], 10, 200, sigma)
         estimate.fit(*(estimate.sample()))
-        string = string + str(name[i]) + "\n"
+        if i % 3 == 0:
+            string = string + str(name[i:3]) + "\n"
         string = string + str(estimate.get_parameters()) + "\n"
 
     write_param(string)
