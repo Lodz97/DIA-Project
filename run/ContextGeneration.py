55cc3c382cf677db416ca8680f12e65526db8a48
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-06-21 16:22:34 +0200

    addition of debug lines

--- run/ContextGeneration.py
+++ run/ContextGeneration.py
@@ -17,19 +17,23 @@ if __name__ == "__main__":
     T, n_experiments, number_week = conf.get_experiment_context_info()
     string_partition = generate_context_feature()
 
-    collected_reward = []
+
     collected_reward_experiment = []
     prob_user_over_experiment = []
 
     for e in range(0, n_experiments):
+        print("Current experiment: " + str(e))
+        collected_reward = []
         environment = dict(zip(string_partition[0], [PricingEnvironment(n_arms=len(arms), probabilities=p) for p in arms_user_prob]))
         context = SuperSetContext(string_partition, arms, 0, 0.95)
         for week in range(0, number_week):
+            print("   Current week: " + str(week))
             n_click = estimate_daily_n_click.n_click_for_days(T)
             click = np.array([sum(el) for el in n_click])  # total number of user each day
             prob_user = weight(np.mean(n_click, axis=0))
             if week != 0:
                 context.select_active_partition()
+                context.print_active_partition()
 
             tmp_reward = []
             for t in range(0, T):
@@ -43,10 +47,12 @@ if __name__ == "__main__":
 
             collected_reward = collected_reward + context.collected_reward()
             prob_user_over_experiment.append(prob_user)
+        print(collected_reward)
         collected_reward_experiment.append(collected_reward)
 
     prob_user_over_experiment = np.mean(prob_user_over_experiment, axis=0)
     opt = 0
     for i in range(0, len(prob_user_over_experiment)):
         opt = opt + np.max(arms_user_prob[i] * arms) * prob_user_over_experiment[i]
+
     plot.plot_cum_regret(opt, collected_reward_experiment)
