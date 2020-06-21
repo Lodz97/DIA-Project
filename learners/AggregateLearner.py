01c4eb574aee1a1b0c53376c5e13aeca20518e40
MattiaBosio <mattia.bosio02@universitadipavia.it>
2020-06-21 15:10:57 +0200

    addition of transaltor attribute to use mapping between learners and string context

--- learners/AggregateLearner.py
+++ learners/AggregateLearner.py
@@ -7,7 +7,9 @@ class AggregateLearner:
     Represents a partition of the context generation algorithm. Each learner of this class represents a context.
     """
     def __init__(self, key_list, arms, confidence):
-        self.__learner = {key: PricingTSLearner(len(arms), arms) for key in key_list}
+        self.__learner = {key: PricingTSLearner(len(arms), arms) for key in range(0, len(key_list))}
+        # mapping learners to corresponding string context
+        self.__translator = {key: key_list[key] for key in range(0, len(key_list))}
         self.__confidence = confidence
 
     def select_learner(self, key_env):
@@ -17,7 +19,7 @@ class AggregateLearner:
         string :return: key for the contest learner
         """
         for key in self.__learner.keys():
-            if key_env in key:
+            if key_env in self.__translator[key]:
                 return key
 
     def pull_arm(self, learner):
@@ -41,18 +43,19 @@ class AggregateLearner:
         """
         float :return: the total reward
         """
-        rewards = [el._collected_reward for el in self.__learner.values()]
+        rewards = [el._collected_rewards for el in self.__learner.values()]
         return sum(rewards)
 
     def number_samples(self):
         n_samples = [learner._round for learner in self.__learner.values()]
-        return {key: n_samples[i] for i, key in enumerate(self.__learner.keys())}
+        return {key: n_samples[key] for key in self.__learner.keys()}
 
     def compute_lower_bound(self):
         samples_for_learner = self.number_samples()
         lower_bound = 0
         total_n_samples = cumsum(samples_for_learner.values())
         for element in samples_for_learner.items():
+            print(element)
             lower_bound += (self.__learner[element[0]].get_reward_best_arm() -
                             sqrt(-log(self.__confidence)/element[1])) * element[1]/total_n_samples
         return lower_bound
